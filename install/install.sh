bd1cec034e3945e123e195aff856bf0c0c378a35
Karl Hobley <karl@torchbox.com>
2015-09-10 09:44:29 +0100

    Don't prebuild wheels
    Going to replace with a pip 7 based solution

--- install/install.sh
+++ install/install.sh
@@ -21,31 +21,12 @@ apt-get install -y openjdk-7-jre-headless
 # Python 2.7
 apt-get install -y python python-dev python-pip python-virtualenv python-wheel
 
-su - vagrant -c "pip2.7 wheel psycopg2==2.6"
-su - vagrant -c "pip2.7 wheel libsass==0.7.0"
-su - vagrant -c "pip2.7 wheel libsass==0.8.2"
-su - vagrant -c "pip2.7 wheel Pillow==2.8.1"
-
-
 # Python 3.4
 apt-get install -y python3 python3-dev python3-pip python3-virtualenv python3-wheel
-
-su - vagrant -c "pip3.4 wheel psycopg2==2.6"
-su - vagrant -c "pip3.4 wheel libsass==0.7.0"
-su - vagrant -c "pip3.4 wheel libsass==0.8.2"
-su - vagrant -c "pip3.4 wheel Pillow==2.8.1"
-
-
-# Tell PIP where to find wheel files
-echo "export PIP_FIND_LINKS=/home/vagrant/wheelhouse" >> /home/vagrant/.bashrc
 
 
 # Create vagrant pgsql superuser
 su - postgres -c "createuser -s vagrant"
-
-
-# Install Fabric and Sphinx
-pip2.7 install Fabric==1.10.1 Sphinx==1.2.3
 
 
 # Elasticsearch
