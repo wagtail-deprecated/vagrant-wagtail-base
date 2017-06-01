9020f2e891d4d3842a54c73b363fa853e4ada711
Karl Hobley <karl@kaed.uk>
2017-06-01 12:39:59 +0100

    Fix capitalisation of Elasticsearch

--- install/install.sh
+++ install/install.sh
@@ -47,7 +47,7 @@ su - postgres -c "createuser -s vagrant"
 
 
 # Elasticsearch
-echo "Downloading ElasticSearch..."
+echo "Downloading Elasticsearch..."
 wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.0.deb
 dpkg -i elasticsearch-5.4.0.deb
 # reduce JVM heap size from 2g to 512m
